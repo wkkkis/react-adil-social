31794863c322cf17ee30520ba437bd7f5beb9b41
Issak <aizekyuy@yandex.ru>
2021-08-12 02:32:34 +0600

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.css';
+
+
+const Header = () =>{
+  return <header className='header'>
+    <img src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg'/>
+  </header>
+}
+ export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Profile = () => {
+  return <div className='content'>
+    <div>
+      <img src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg' />
+    </div>
+    <div>
+      ava+ description
+    </div>
+    <div>
+      My post
+    </div>
+    <div>
+      New post
+    </div>
+    <div className='post'>
+      <div className='item'>
+        post1
+      </div>
+      <div className='item'>
+        post2
+      </div>
+    </div>
+  </div>;
+};
+
+export default Profile;
